c301436fdc538c7b5247090041a44276ff601eb6
Age Mooij <age.mooij@gmail.com>
2012-12-29 22:43:50 +0100

    Added some new specs2+akka test utils

--- src/test/scala/com/scalapenos/riak/TestUtils.scala
+++ src/test/scala/com/scalapenos/riak/TestUtils.scala
@@ -1,10 +1,22 @@
 package com.scalapenos.riak
 
 import org.specs2.mutable._
+import org.specs2.specification.{Fragments, Step}
+import org.specs2.time.NoTimeConversions
 
 import akka.actor._
 import akka.testkit._
 
 abstract class AkkaTestkitContext extends TestKit(ActorSystem()) with ImplicitSender with After {
   def after = system.shutdown()
+}
+
+trait AkkaActorSystemSpecification extends Specification with NoTimeConversions {
+  implicit val system = ActorSystem(actorSystemNameFrom(getClass))
+  implicit val executor = system.dispatcher
+
+  /* Add a final step to the list of test fragments that shuts down the actor system. */
+  override def map(fs: => Fragments) = super.map(fs).add(Step(system.shutdown))
+
+  private def actorSystemNameFrom(clazz: Class[_]) = clazz.getName.replace('.', '-').filter(_ != '$')
 }
