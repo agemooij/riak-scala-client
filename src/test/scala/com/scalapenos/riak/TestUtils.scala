3882616f554b9a3a97fd192e3b6c0e132156cc1a
Age Mooij <age.mooij@gmail.com>
2012-12-29 23:48:40 +0100

    Skeleton code for WIP specs2 test helpers for matching against Futures

--- src/test/scala/com/scalapenos/riak/TestUtils.scala
+++ src/test/scala/com/scalapenos/riak/TestUtils.scala
@@ -11,6 +11,7 @@ abstract class AkkaTestkitContext extends TestKit(ActorSystem()) with ImplicitSe
   def after = system.shutdown()
 }
 
+
 trait AkkaActorSystemSpecification extends Specification with NoTimeConversions {
   implicit val system = ActorSystem(actorSystemNameFrom(getClass))
   implicit val executor = system.dispatcher
@@ -20,3 +21,14 @@ trait AkkaActorSystemSpecification extends Specification with NoTimeConversions 
 
   private def actorSystemNameFrom(clazz: Class[_]) = clazz.getName.replace('.', '-').filter(_ != '$')
 }
+
+
+trait Specs2FutureMatchers {
+  import org.specs2.matcher.Matcher
+  import org.specs2.matcher.Matchers._
+  import scala.concurrent._
+
+
+
+}
+
