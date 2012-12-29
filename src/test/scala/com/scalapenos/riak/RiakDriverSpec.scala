8074ae63fb643d350b6aa53dcd79f75998c9b439
Age Mooij <age.mooij@gmail.com>
2012-12-29 23:47:50 +0100

    Some very basic test code

--- src/test/scala/com/scalapenos/riak/RiakDriverSpec.scala
+++ src/test/scala/com/scalapenos/riak/RiakDriverSpec.scala
@@ -1,17 +1,26 @@
 package com.scalapenos.riak
 
+import scala.concurrent._
+import scala.concurrent.duration._
+
 import akka.actor._
 
 import spray.json._
 
 
 class RiakDriverDataStoresSpec extends AkkaActorSystemSpecification {
-  // val random = new util.Random()
-  // def randomUserId = random.nextLong().abs.toString
+  case class Kitten(name: String, cuteness: Int)
+  object Kitten extends DefaultJsonProtocol {
+    implicit val jsonFormat = jsonFormat2(Kitten.apply)
+  }
 
   "The riak driver" should {
     "be able to perform a simple get-put-get-delete-get CRUD flow" in {
-      pending
+      val client = RiakClient(system)
+      val connection = client.connect()
+      val bucket = connection.bucket[Kitten]("test")
+
+      Await.result(bucket.fetch("Nano"), 5 seconds) must beNone
     }
   }
 
